fc0d38044c9f49565c74a820c681dd35cb129e10
kushalpatel1999 <40886542+kushalpatel1999@users.noreply.github.com>
2020-08-27 08:12:21 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -115,18 +115,6 @@ class Register extends Component {
               {errors.fullName.length > 0 && 
                 <span className='error'>{errors.fullName}</span>}
             </div>
-
-
-            <div class="dropdown">
-              <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown
-              <span class="caret"></span></button>
-              <ul class="dropdown-menu">
-              <li><a href="/">HTML</a></li>
-              <li><a href="/">CSS</a></li>
-              <li><a href="/">JavaScript</a></li>
-              </ul>
-            </div>
-            
             
             <div className='email'>
               <label htmlFor="email">Email</label>
